e8c3f87e8682b297bfca4cb4aee18f94ea0c3d4a
mistborns <98649890+mistborns@users.noreply.github.com>
2022-10-13 09:20:49 +0530

    Add files via upload

---
+++ DS/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main()
+{
+    int size,temp;
+    printf("enter the size of array:");
+    scanf("%d",&size);
+    int arr[size];
+    for(int i = 0; i < size; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("array is:\n");
+    for(int i = 0; i < size; i++)
+    {
+        printf("%d\t",arr[i]);
+    }
+    for(int i = 0; i < size - 1; i++)
+    {
+        for(int j = 0; j < size - 1; j++)
+        {
+            if(arr[j] > arr[j + 1])
+            {
+                temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+                
+            }
+        }
+    }
+    printf("the sorted array is:\n");
+    for(int i = 0; i < size; i++)
+    {
+        printf("%d\t",arr[i]);
+    }
+
+    return 0;
+}
