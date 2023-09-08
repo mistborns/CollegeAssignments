97c338abd3c78c9dd16c10d9950fe59db4c9e811
mistborns <98649890+mistborns@users.noreply.github.com>
2023-09-08 16:32:46 +0530

    Create selection_sort.cpp

---
+++ DAA/selection_sort.cpp
@@ -0,0 +1,37 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    int n, count = 0;
+    cin>>n;int arr[n];
+    for(int i =0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i = 0; i < n ; i++)
+    {
+     count = count + 2;   
+        for(int j = i + 1; j < n ; j++)
+        {
+            count = count + 3 ;
+            if(arr[i] > arr[j])
+            {
+                count++;
+                int temp = arr[i];
+                count++;
+                arr[i] = arr[j];
+                count++;
+                arr[j] = temp;
+                count++;
+            }
+        }
+    }
+    count = count + 4;
+for(int i = 0; i < n; i++)
+{
+    cout<<arr[i]<<"\n";
+    count = count + 3;
+}
+
+cout<<count + 2;    return 0;
+}
